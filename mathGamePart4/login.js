0491fa1e06012a6a35cc8f20792fefff10987ac6
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-01-19 19:50:11 -0500

    Add files via upload

---
+++ mathGamePart4/login.js
@@ -0,0 +1,10 @@
+	
+function login(){
+	player1_name = localStorage.getItem("player1_name");
+	player2_name = localStorage.getItem("player2_name");
+
+	player1_score = 0;
+	player2_score = 0;
+	 window.location = "quiz2.html"
+}
+	
